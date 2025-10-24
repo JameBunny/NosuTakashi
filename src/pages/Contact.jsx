1d6560ce578a7f8a1043a5251bc51d882430e14c
James XO Arena <224217651+JameBunny@users.noreply.github.com>
2025-10-25 04:23:17 +0700

    Delete src directory

--- src/pages/Contact.jsx
+++
@@ -1,8 +0,0 @@
-export default function Contact(){
-  return (
-    <section className="max-w-6xl mx-auto px-4 py-10">
-      <h2 className="text-2xl font-bold mb-4">Contact</h2>
-      <p className="text-neutral-300">Email: nosutakashiofficial@gmail.com</p>
-    </section>
-  )
-}
