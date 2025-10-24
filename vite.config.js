f8d3e3fec70330a58fd1a4c1737cf9526501f1a9
James XO Arena <224217651+JameBunny@users.noreply.github.com>
2025-10-25 04:12:33 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  // ถ้าดีพลอยเป็น <username>.github.io/<repo> ให้เปิดบรรทัดนี้และใส่ชื่อ repo
+  // ถ้า repo เป็น <username>.github.io/<repo> ให้เปิดบรรทัดนี้
   // base: '/<repo>/',
+  build: { outDir: 'docs' } // ← ให้ build ลงโฟลเดอร์ docs
 })
