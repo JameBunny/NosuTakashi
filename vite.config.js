afa16eb2f71a4611a4058dea3f9cdea3fe4872d2
James XO Arena <224217651+JameBunny@users.noreply.github.com>
2025-10-25 04:23:57 +0700

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  plugins: [react()],
-  // ถ้า repo เป็น <username>.github.io/<repo> ให้เปิดบรรทัดนี้
-  // base: '/<repo>/',
-  build: { outDir: 'docs' } // ← ให้ build ลงโฟลเดอร์ docs
-})
