29605a04c1bdb6ab1d420392a782f9f32c2e05ee
James XO Arena <224217651+JameBunny@users.noreply.github.com>
2025-10-25 04:04:50 +0700

    Create About.jsx

---
+++ src/pages/About.jsx
@@ -0,0 +1,8 @@
+export default function About(){
+  return (
+    <section className="max-w-6xl mx-auto px-4 py-10">
+      <h2 className="text-2xl font-bold mb-4">About</h2>
+      <p className="text-neutral-300">About Nosu Horror Hub.</p>
+    </section>
+  )
+}
